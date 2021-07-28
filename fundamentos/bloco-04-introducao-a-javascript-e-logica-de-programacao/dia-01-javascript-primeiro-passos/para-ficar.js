3c82f01fda10656c7c93414fb8f1f190a4414409
MuriloAmaral <muralha2000@gmail.com>
2021-07-28 15:36:32 -0300

    para-fixar1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiro-passos/para-ficar.js
@@ -0,0 +1,11 @@
+const myName = "Murilo";
+const birthCity = "Cataguases";
+let birthYear = 2000;
+birthYear = 2030;
+birthCity = "Nova Iorque";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+
