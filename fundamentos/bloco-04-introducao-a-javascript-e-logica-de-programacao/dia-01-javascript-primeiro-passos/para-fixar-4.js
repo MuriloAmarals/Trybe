adc3db6042a4ac7a971671d514b6f898af9294da
MuriloAmaral <muralha2000@gmail.com>
2021-07-28 16:33:17 -0300

    para-fixar4

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiro-passos/para-fixar-4.js
@@ -0,0 +1,18 @@
+let candidato = 'aprovada';
+
+switch (candidato){
+    case 'aprovada':
+    console.log('aprovada');
+    break;
+
+    case 'reprovada':
+    console.log('reprovada');
+    break;
+
+    case 'lista':
+    console.log(lista);
+    break;
+
+    default:
+    console.log('não se aplica');
+}
