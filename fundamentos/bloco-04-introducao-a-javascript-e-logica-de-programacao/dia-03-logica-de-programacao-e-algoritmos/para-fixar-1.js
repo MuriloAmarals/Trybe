71473edc0a89a7ef6afb92dcd3218f8586e55a41
MuriloAmaral <muralha2000@gmail.com>
2021-07-30 15:59:31 -0300

    para fixar1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/para-fixar-1.js
@@ -0,0 +1,67 @@
+//Baby Steps
+
+//Exercicio 1
+let fruits = [3, 4, 10, 1, 12];
+
+//Exercicio 2
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+//Exercicio 3
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+
+}
+
+//Exercicio 4
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index];
+}
+
+//Exercicio 5
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index];
+}
+
+if (sum > 15) {
+
+} else {
+
+}
+
+//Exercicio 6
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index];
+}
+
+if(sum > 15) {
+  console.log(sum);
+} else {
+
+}
+
+//Exercicio 7
+let fruits = [3, 4, 10, 1, 12];
+let sum = 0;
+
+for (let index = 0; index < fruits.length; index += 1) {
+  sum += fruits[index];
+}
+
+if (sum > 15) {
+  console.log(sum);
+} else {
+  console.log('valor menor que 16');
+}
+
