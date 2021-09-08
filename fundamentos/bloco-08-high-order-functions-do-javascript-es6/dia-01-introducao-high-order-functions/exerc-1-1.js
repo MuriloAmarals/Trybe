9c7c21dfe357a73ef4c11e28a5f6a80cfbae269c
MuriloAmaral <muralha2000@gmail.com>
2021-09-08 10:02:03 -0300

    dia 8.1

---
+++ fundamentos/bloco-08-high-order-functions-do-javascript-es6/dia-01-introducao-high-order-functions/exerc-1-1.js
@@ -0,0 +1,17 @@
+const gerador = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return { fullName, email: `${email}@trybe.com` };
+  };
+  
+  const novosEmpregados = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'),
+      id2: callback('Luiza Drumond'),
+      id3: callback('Carla Paiva'),
+    };
+    return employees;
+  };
+  
+  console.log(novosEmpregados(gerador));
+
+  
