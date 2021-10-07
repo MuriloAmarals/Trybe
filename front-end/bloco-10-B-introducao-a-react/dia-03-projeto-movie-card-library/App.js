81c653fd4d4154d65ac846c8bd42bca6368022b6
MuriloAmaral <muralha2000@gmail.com>
2021-10-07 18:13:24 -0300

    dia novo

---
+++ front-end/bloco-10-B-introducao-a-react/dia-03-projeto-movie-card-library/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Headers from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
+
+function App() {
+  return (
+    <div>
+      <Headers />
+      <MovieList movies={ movies } />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/bloco-10-B-introducao-a-react/dia-03-projeto-movie-card-library/components/MovieCard.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div className="movie-card movie-card-body ">
+        <img
+          className="movie-card-image"
+          src={ movie.imagePath }
+          alt={ movie.subtitle }
+        />
+        <h4 className="movie-card-title">
+          {movie.title}
+        </h4>
+        <h5 className="movie-card-subtitle">
+          {movie.subtitle}
+        </h5>
+        <p className="movie-card-storyline">
+          {movie.storyline}
+        </p>
+        <Rating rating={ movie.rating } />
+      </div>);
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
